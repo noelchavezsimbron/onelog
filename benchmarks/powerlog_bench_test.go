6bf663289a14094fa891e0dc4246509913359f40
Alexander <leonxnadercs@gmail.com>
2020-11-23 14:17:48 -0500

    test passed 100%

--- benchmarks/powerlog_bench_test.go
+++ benchmarks/powerlog_bench_test.go
@@ -1,7 +1,7 @@
 package benchmarks
 
 import (
-	"github.com/francoispqt/onelog/_onelog"
+	"github.com/noelchavezsimbron/powerlog"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -9,11 +9,11 @@ import (
 )
 
 func TestPrint(t *testing.T) {
-	logger := _onelog.New(os.Stdout, _onelog.ALL).
-		Hook(func(e _onelog.Entry) {
+	logger := powerlog.New(os.Stdout, powerlog.ALL).
+		Hook(func(e powerlog.Entry) {
 			e.Int64("time", time.Now().Unix())
 		})
-	logger.InfoWith("message").
+	logger.InfoWith().
 		String("test", "test").
 		String("test", "test").
 		String("test", "test").
@@ -26,8 +26,8 @@ func TestPrint(t *testing.T) {
 
 func BenchmarkOnelog(b *testing.B) {
 	b.Run("with-fields", func(b *testing.B) {
-		logger := _onelog.New(ioutil.Discard, _onelog.ALL).
-			Hook(func(e _onelog.Entry) {
+		logger := powerlog.New(ioutil.Discard, powerlog.ALL).
+			Hook(func(e powerlog.Entry) {
 				e.Int64("time", time.Now().Unix())
 			})
 		s := struct {
@@ -36,7 +36,7 @@ func BenchmarkOnelog(b *testing.B) {
 		b.ResetTimer()
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
-				logger.InfoWithFields("message", func(e _onelog.Entry) {
+				logger.InfoWithFields("message", func(e powerlog.Entry) {
 					e.Int("test", s.i)
 					e.String("test", "test")
 					e.String("test", "test")
@@ -50,8 +50,8 @@ func BenchmarkOnelog(b *testing.B) {
 		})
 	})
 	b.Run("message-only", func(b *testing.B) {
-		logger := _onelog.New(ioutil.Discard, _onelog.ALL).
-			Hook(func(e _onelog.Entry) {
+		logger := powerlog.New(ioutil.Discard, powerlog.ALL).
+			Hook(func(e powerlog.Entry) {
 				e.Int64("time", time.Now().Unix())
 			})
 		b.ResetTimer()
@@ -62,26 +62,26 @@ func BenchmarkOnelog(b *testing.B) {
 		})
 	})
 	b.Run("entry-message-only", func(b *testing.B) {
-		logger := _onelog.New(ioutil.Discard, _onelog.ALL).
-			Hook(func(e _onelog.Entry) {
+		logger := powerlog.New(ioutil.Discard, powerlog.ALL).
+			Hook(func(e powerlog.Entry) {
 				e.Int64("time", time.Now().Unix())
 			})
 		b.ResetTimer()
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
-				logger.InfoWith("message").Write()
+				logger.InfoWith().Write()
 			}
 		})
 	})
 	b.Run("entry-fields", func(b *testing.B) {
-		logger := _onelog.New(ioutil.Discard, _onelog.ALL).
-			Hook(func(e _onelog.Entry) {
+		logger := powerlog.New(ioutil.Discard, powerlog.ALL).
+			Hook(func(e powerlog.Entry) {
 				e.Int64("time", time.Now().Unix())
 			})
 		b.ResetTimer()
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
-				logger.InfoWith("message").
+				logger.InfoWith().
 					String("test", "test").
 					String("test", "test").
 					String("test", "test").
@@ -95,8 +95,8 @@ func BenchmarkOnelog(b *testing.B) {
 	})
 
 	b.Run("accumulated context", func(b *testing.B) {
-		logger := _onelog.New(ioutil.Discard, _onelog.ALL).
-			With(func(e _onelog.Entry) {
+		logger := powerlog.New(ioutil.Discard, powerlog.ALL).
+			With(func(e powerlog.Entry) {
 				e.Int("int", 1)
 			})
 		b.ResetTimer()
--- log/main.go
+++ log/main.go
@@ -1,12 +1,12 @@
 package log
 
 import (
-	"github.com/francoispqt/onelog/_onelog"
+	"github.com/noelchavezsimbron/powerlog"
 	"os"
 	"time"
 )
 
-var logger = _onelog.New(os.Stdout, _onelog.ALL).Hook(func(e _onelog.Entry) {
+var logger = powerlog.New(os.Stdout, powerlog.ALL).Hook(func(e powerlog.Entry) {
 	e.Int64("time", time.Now().Unix())
 })
 
@@ -16,7 +16,7 @@ func Info(msg string) {
 }
 
 // InfoWithFields prints a message with log level INFO and fields.
-func InfoWithFields(msg string, fields func(e _onelog.Entry)) {
+func InfoWithFields(msg string, fields func(e powerlog.Entry)) {
 	logger.InfoWithFields(msg, fields)
 }
 
@@ -26,7 +26,7 @@ func Debug(msg string) {
 }
 
 // DebugWithFields prints a message with log level DEBUG and fields.
-func DebugWithFields(msg string, fields func(e _onelog.Entry)) {
+func DebugWithFields(msg string, fields func(e powerlog.Entry)) {
 	logger.DebugWithFields(msg, fields)
 }
 
@@ -36,7 +36,7 @@ func Warn(msg string) {
 }
 
 // WarnWithFields prints a message with log level WARN and fields.
-func WarnWithFields(msg string, fields func(e _onelog.Entry)) {
+func WarnWithFields(msg string, fields func(e powerlog.Entry)) {
 	logger.WarnWithFields(msg, fields)
 }
 
@@ -46,7 +46,7 @@ func Error(msg string) {
 }
 
 // ErrorWithFields prints a message with log level ERROR and fields.
-func ErrorWithFields(msg string, fields func(e _onelog.Entry)) {
+func ErrorWithFields(msg string, fields func(e powerlog.Entry)) {
 	logger.ErrorWithFields(msg, fields)
 }
 
@@ -56,6 +56,6 @@ func Fatal(msg string) {
 }
 
 // FatalWithFields prints a message with log level FATAL and fields.
-func FatalWithFields(msg string, fields func(e _onelog.Entry)) {
+func FatalWithFields(msg string, fields func(e powerlog.Entry)) {
 	logger.FatalWithFields(msg, fields)
 }
